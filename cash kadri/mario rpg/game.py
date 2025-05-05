e27988b59f1277beab0129be0e055c1c71da57c1
Shawn <163067793+Teriyaki440@users.noreply.github.com>
2025-05-05 16:02:40 -0500

    cash session 5/5

--- cash kadri/mario rpg/game.py
+++ cash kadri/mario rpg/game.py
@@ -37,17 +37,61 @@ def draw_player():
     #we draw eyebrows
     eyebrow_left_x = -30
     eyebow_top_y = -10
-    eybrow_width = 29
+    eyebrow_width = 29
     eyebrow_height = 10
-    left_eyebrow = pygame.Rect(p1_pstn.x + eyebrow_left_x, p1_pstn.y + eyebow_top_y, eybrow_width, eyebrow_height)
+    left_eyebrow = pygame.Rect(p1_pstn.x + eyebrow_left_x, p1_pstn.y + eyebow_top_y, eyebrow_width, eyebrow_height)
     pygame.draw.rect(SCREEN, BLACK,  left_eyebrow)
 
     point_list = []
-    point_list.append(pygame.math.Vector2(p1_pstn.x, p1_pstn.y - 5)) # point #0
-    point_list.append(pygame.math.Vector2(point_list[0].x+7, point_list[0].y-7)) # point #1
-    point_list.append(pygame.math.Vector2(point_list[1].x+15,point_list[1].y+4)) # point #2
-    #TODO come back add 3rd point
+
+    # 0 - blue
+    point_list.append(pygame.math.Vector2(
+        p1_pstn.x,
+        p1_pstn.y - 2)) 
+    
+    # 1 - purple
+    point_list.append(pygame.math.Vector2(
+        point_list[0].x + eyebrow_width/4,
+        p1_pstn.y-17))
+
+    # 2 - red
+    point_list.append(pygame.math.Vector2(
+        point_list[0].x + eyebrow_width/2,
+        p1_pstn.y - 19)) 
+    
+    # 3 - ORANGE
+    point_list.append(pygame.math.Vector2(
+        point_list[0].x + eyebrow_width/4*3,
+        p1_pstn.y-17))
+
+    # 4 - green
+    point_list.append(pygame.math.Vector2(
+        point_list[0].x + eyebrow_width,
+        p1_pstn.y - 2)) 
+    
+    # 5 - CYAN
+    point_list.append(pygame.math.Vector2(
+        point_list[3].x,
+        point_list[4].y))
+
+    # 6 - yellow
+    point_list.append(pygame.math.Vector2(
+        point_list[0].x + eyebrow_width/2,
+        p1_pstn.y - 10))
+    
+    #-7 GRAY
+    point_list.append(pygame.math.Vector2(
+        point_list[1].x,
+        point_list[0].y))
+
     pygame.draw.polygon(SCREEN,BLACK,point_list)
+
+    #draw da eyes 
+    eye_left=pygame.math.Vector2(
+        p1_pstn.x-p1_size/3,
+        p1_pstn.y+4
+    )
+    pygame.draw.circle(SCREEN,BLACK,eye_left,7)
 
 #mr. enemy variables 
 enemy_pstn = pygame.math.Vector2(200, 100)
