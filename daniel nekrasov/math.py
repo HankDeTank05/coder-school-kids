7217891c802f741a39d2d224714ac28a34310285
Shawn <163067793+Teriyaki440@users.noreply.github.com>
2025-05-11 12:02:34 -0500

    rylan session 5/11

--- daniel nekrasov/math.py
+++ daniel nekrasov/math.py
@@ -2,7 +2,7 @@ import math
 import pygame
 
 #                   center of circle 1             radius of circle 1
-#                   should be a vector2           should be a number     (same tpe as center1)       (same type as radius1)   this function returns boolean data (true or false)
+#                   should be a vector2           should be a number     (same type as center1)      (same type as radius1)   this function returns boolean data (true or false)
 #                  ||||||||||||||||||||||||||||  ||||||||||||||||||||  ||||||||||||||||||||||||||||  ||||||||||||||||||||     ||||
 #                  vvvvvvvvvvvvvvvvvvvvvvvvvvvv  vvvvvvvvvvvvvvvvvvvv  vvvvvvvvvvvvvvvvvvvvvvvvvvvv  vvvvvvvvvvvvvvvvvvvv     vvvv
 def circle_collide(center1: pygame.math.Vector2, radius1: int | float, center2: pygame.math.Vector2, radius2: int | float) -> bool:
--- rylan ali/foodstuff.py
+++ rylan ali/foodstuff.py
@@ -32,14 +32,33 @@ class FoodManager:
         else:
             print("List is full")
    
-    def update(self, other_rect: pygame.Rect):
-        # TODO: come back here and rewrite this code
-        food_2_remove = []
+    def update(self, snake_rect: pygame.Rect):
+
+        # generating a list of indices at which to remove Food objects
+
+        # this just means the variable is a list of ints
+        #            vvvvvvvvvvv
+        food_2_remove: list[int] = []
+
+        # this for loop iterates over self.food in forward order 
+        # when a food object collides with snake_rect it adds an index to food_2_remove
         for i in range(len(self.food)):
-            if self.food[i].rect.colliderect(other_rect) == True:
+            food_item = self.food[i]
+            food_rect = food_item.rect
+            if food_rect.colliderect(snake_rect) == True:
                 food_2_remove.append(i)
-        while len(food_2_remove) > 0:
-            pass
+
+        # removes Food items that the player has collided with
+
+        food_2_remove = sorted(food_2_remove)
+
+        # this for loop iterates over food_2_remove in end-to-start order (aka, reverse order)
+        # it iterates in reverse order so it doesen't affect the other indices so we won't remove the wrong items
+        for remove_index in range(start=len(food_2_remove) - 1, stop=-1, step=-1):
+            # remove_index = the index of food_2_remove
+            # food_index = the index at which to remove a Food object (from self.food)
+            food_index = food_2_remove[remove_index]
+            self.food.pop(food_index)
 
     def draw(self, screen):
         for i in range(len(self.food)):
