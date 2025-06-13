c54b250a2ef69816a964bcd38fbbf9f61d1e2b82
Shawn <163067793+Teriyaki440@users.noreply.github.com>
2025-06-13 16:27:17 -0500

    zoe session

---
+++ zoe watson/puzzle_game/easy.py
@@ -0,0 +1,32 @@
+def puzzle1():
+    print ("What goes up but never comes down?")
+    response = input("What is it?")
+    res = response.upper()
+    if res == "AGE":
+        print ("Correct-o moving on...")
+
+
+def puzzle2():
+    print ("what has 88 keys but doesn't open any doors?")
+    response = input("What is it?")
+    res = response.upper()
+    if res == "A PIANO":
+        print ("Correct-o moving on")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ zoe watson/puzzle_game/main.py
@@ -0,0 +1,84 @@
+import easy
+
+
+print("choose a [puzzle then select the level. Hope you struggle. :)")
+print("1. EASY\n 2. MEDIUM\n 3. HARD\n 4. IMPOSSIBLE")
+
+
+response = input("What level would you like?")
+response = int(response)
+
+
+if response == 1:
+    easy.puzzle2()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#I shall start here
