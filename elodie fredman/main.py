b0ad190419ac7abedb7f1456140d87ee929435dd
HankDeTank05 <henryhankholman@gmail.com>
2025-08-24 11:01:54 -0500

    elodie session 8/24

--- elodie fredman/main.py
+++ elodie fredman/main.py
@@ -6,7 +6,95 @@ import random
 import pygame
 
 # project imports (my code)
-import gamemath
+# none yet
+
+#######################
+# GAME MATH FUNCTIONS #
+#######################
+
+def circle_overlap(x1, y1, r1, x2, y2, r2) -> bool:
+    distance: float = math.hypot(x1 - x2, y1 - y2)
+    overlap: bool = distance < r1 + r2
+    return overlap
+
+####################
+# PLAYER FUNCTIONS #
+####################
+
+def player_update(frame_time):
+    global circle_x, circle_y
+    # Establishing the movement of the player
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        circle_y -= speed * frame_time
+    if keys[pygame.K_DOWN]:
+        circle_y += speed * frame_time
+    if keys[pygame.K_RIGHT]:
+        circle_x += speed * frame_time
+    if keys[pygame.K_LEFT]:
+        circle_x -= speed * frame_time
+
+    # Keep player on screen
+    circle_x = max(circle_radius, min(WIDTH - circle_radius, circle_x))
+    circle_y = max(circle_radius, min(HEIGHT - circle_radius, circle_y))
+
+def player_draw():
+    pygame.draw.circle(screen, (245, 45, 67), (int(circle_x), int(circle_y)), circle_radius)
+
+##################
+# FOOD FUNCTIONS #
+##################
+
+def food_update(food, frame_time):
+    fx, fy, fr, color = food
+
+def food_draw(food):
+    fx, fy, fr, color = food
+    pygame.draw.circle(screen, color, (int(fx), int(fy)), fr) # Puts food on screen / draws food
+
+#############################
+# FOOD MANAGEMENT FUNCTIONS #
+#############################
+
+def create_new_food():
+    global foods
+    while True:
+        # STEP 0: generate new food
+        new_radius = random.randint(10, 80)
+        new_x = random.randint(new_radius, WIDTH - new_radius)
+        new_y = random.randint(new_radius, HEIGHT - new_radius)
+        new_color = (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255)
+        )
+        overlap = False
+        # STEP 1. check if the newly created food overlaps with ANY existing food
+        for index in range(len(foods)):
+            food = foods[index]
+            fx = food[0]
+            fy = food[1]
+            fr = food[2]
+            overlap = circle_overlap(x1=fx, y1=fy, r1=fr, x2=new_x, y2=new_y, r2=new_radius)
+            if overlap == True:
+                break
+        
+        # 1a. if it does, re-run the WHILE loop
+        if overlap == True:
+            continue
+
+        # 1b. if it does not, exit the loop
+        elif overlap == False:
+            break
+    foods.append([new_x, new_y, new_radius, new_color])
+
+def food_update_all(frame_time):
+    for food in foods:
+        food_update(food=food, frame_time=frame_time)
+
+def food_draw_all():
+    for food in foods:
+        food_draw(food=food)
 
 # Initialize Pygame and Mixer
 pygame.init()
@@ -46,23 +134,10 @@ foods = [
     [684, 547, 109, (0, 247, 255)],
 ]
 
-def create_new_food():
-    new_radius = random.randint(10, 80)
-    new_x = random.randint(new_radius, WIDTH - new_radius)
-    new_y = random.randint(new_radius, HEIGHT - new_radius)
-    new_color = (
-        random.randint(0, 255),
-        random.randint(0, 255),
-        random.randint(0, 255)
-    )
-    while True:
-        #1. check if the newly created food overlaps with ANY existing food
-        #2a. if it does, re-generate a new food, and re-run the loop
-        #2b. if it does not, exit the loop
-        pass
-    foods.append([new_x, new_y, new_radius, new_color])
+#############
+# Game Loop #
+#############
 
-# Game Loop
 while running:
     frame_time = clock.tick(60) / 1000  # 60 FPS
     screen.fill((89, 134, 203))  # Background color
@@ -84,35 +159,31 @@ while running:
         screen.blit(prompt_text, ((WIDTH - prompt_text.get_width()) // 2, HEIGHT // 2))
 
     elif game_state == "playing":
-        # Establishing the movement of the player
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            circle_y -= speed * frame_time
-        if keys[pygame.K_DOWN]:
-            circle_y += speed * frame_time
-        if keys[pygame.K_RIGHT]:
-            circle_x += speed * frame_time
-        if keys[pygame.K_LEFT]:
-            circle_x -= speed * frame_time
 
-        # Keep player on screen
-        circle_x = max(circle_radius, min(WIDTH - circle_radius, circle_x))
-        circle_y = max(circle_radius, min(HEIGHT - circle_radius, circle_y))
+        ########################
+        # step 1: update stuff #
+        ########################
+
+        player_update(frame_time=frame_time)
+        food_update_all(frame_time=frame_time)
 
         # Draw food and check collisions
         for food in foods:
             fx, fy, fr, color = food
-            pygame.draw.circle(screen, color, (int(fx), int(fy)), fr) # Puts food on screen / draws food
             #  Checks if cirlces overlap                                                If player circle is larger than food circle
             #  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv     vvvvvvvvvvvvvvvvvvvv
-            if gamemath.circle_overlap(circle_x, circle_y, circle_radius,fx, fy,fr) and circle_radius >= fr:
+            if circle_overlap(circle_x, circle_y, circle_radius,fx, fy,fr) and circle_radius >= fr:
                 eat_sound.play()
                 circle_radius += int(fr * 0.5)
                 foods.remove(food)
                 create_new_food()
 
-        # Draw player
-        pygame.draw.circle(screen, (245, 45, 67), (int(circle_x), int(circle_y)), circle_radius)
+        ######################
+        # step 2: draw stuff #
+        ######################
+
+        player_draw()
+        food_draw_all()
 
     pygame.display.flip()
 
---
+++ elodie fredman/today.py
@@ -0,0 +1,5 @@
+'''
+(DONE) 1. finish the create_new_food function
+2. create player_update
+3. create player_draw
+'''
