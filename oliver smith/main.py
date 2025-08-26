136a6221d46430438003721cde8f058b6f783973
HankDeTank05 <henryhankholman@gmail.com>
2025-08-26 18:05:39 -0500

    oliver session 8/26

--- oliver smith/main.py
+++ oliver smith/main.py
@@ -6,15 +6,165 @@ import time
 import pygame
 
 # project imports (this is our stuff we created)
-import common as c
-import player as p
-import obstacle as o
+
 
 pygame.init()
 pygame.font.init()
 
-# create colors
+##################
+# GAME CONSTANTS #
+##################
 
+# screen constants
+WIDTH = 500
+HEIGHT = 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dodge the Obstacle")
+
+# COLORS
+WHITE = (255, 255, 255)
+RED = (200, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 240, 225)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 100, 0)
+
+# game stuff
+PLAYER_MAX_HEALTH = 700
+OBSTACLE_DMG = 100
+
+# HUD stuff
+HEALTH_BAR_WIDTH = 400
+HEALTH_BAR_HEIGHT = 50
+
+################
+# GAME CLASSES #
+################
+
+class Player:
+
+    # constructor
+    def __init__(self):
+        size = pygame.math.Vector2(50, 50)
+        pos = pygame.math.Vector2(WIDTH // 2 - size.x // 2,
+                                       HEIGHT - size.y - 10)
+        self._speed: int | float = 350
+        self._rect: pygame.Rect = pygame.Rect(pos, size)
+        self._max_hp = PLAYER_MAX_HEALTH
+        self._hp = self._max_hp
+
+    # Game functions
+
+    def update(self, frame_time):
+
+        ###################
+        # move the player #
+        ###################
+
+        keys = pygame.key.get_pressed()
+        move_x = 0
+        move_y = 0
+        if keys[pygame.K_UP]:
+            move_y -= 1
+        if keys[pygame.K_DOWN]:
+            move_y += 1
+        if keys[pygame.K_LEFT]:
+            move_x -= 1
+        if keys[pygame.K_RIGHT]:
+            move_x += 1
+        player_move = pygame.math.Vector2(move_x, move_y)
+        # correct diagonal movement
+        if player_move.length() > 0:
+            player_move = player_move.normalize() * self._speed * frame_time
+
+        # move the rectangle
+        self._rect.move_ip(player_move)
+
+        ##############################
+        # Keeps player in the screen #
+        ##############################
+
+        # If player is partialy or totaly off screen on the left
+        if self._rect.left < 0:
+            self._rect.left = 0
+
+        # If player is partialy or totaly off screen on the right
+        elif self._rect.right >= WIDTH:
+            self._rect.right = WIDTH - 1
+
+        # If player is partialy or totaly off screen on the bottom
+        if self._rect.bottom >= HEIGHT:
+            self._rect.bottom = HEIGHT - 1
+
+    def draw(self):
+        pygame.draw.rect(SCREEN, BLUE, self._rect)
+
+    # getters
+
+    def get_pos(self) -> pygame.math.Vector2:
+        return pygame.math.Vector2(self._rect.topleft)
+    
+    def get_hp(self) -> int:
+        return self._hp
+
+    # setters
+
+    def set_pos(self, new_pos: pygame.math.Vector2):
+        self._rect.update(new_pos, self._rect.size)
+
+    def set_x(self, new_x: int | float):
+        self.set_pos(pygame.math.Vector2(new_x, self._rect.y))
+
+    def set_y(self, new_y: int | float):
+        self.set_pos(pygame.math.Vector2(self._rect.x, new_y))
+
+    def set_speed(self, new_speed: int | float):
+        self._speed = new_speed
+
+    def change_speed(self, speed_change: int | float):
+        self._speed += speed_change
+
+    def take_damage(self, damage: int):
+        self._hp -= damage
+
+class Obstacle:
+
+    # costructor
+    def __init__(self, width, height, speed, explodey):
+        self._rect = pygame.Rect(random.randint(0, WIDTH - width), -height,
+                                 width, height)
+        self._speed = speed
+        self._explodey = explodey
+
+    def update(self, frame_time, slow_down_timer):
+        # move the obstacle
+        if slow_down_timer > 0:
+            self._rect.move_ip(0, self._speed * frame_time / 4)
+            # TODO: set the obstacle spawn delay to 2
+        else:
+            self._rect.move_ip(0, self._speed * frame_time)
+
+        if self._rect.y > HEIGHT:
+            pass
+
+class Hud:
+
+    def __init__(self):
+        self._max_health_bar_width = HEALTH_BAR_WIDTH
+        self._health_pcent = 1
+
+    def update(self, frame_time):
+        self._health_pcent = player.get_hp() / PLAYER_MAX_HEALTH
+
+    def draw(self):
+        # draw the background rectangle
+        bg_rect = pygame.Rect((0, 0), (HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+        bg_rect.midbottom = pygame.math.Vector2(WIDTH/2, HEIGHT-1)
+        pygame.draw.rect(SCREEN, BLACK, bg_rect)
+        # draw the current health rectangle
+        hp_rect = pygame.Rect(bg_rect.topleft, (self._health_pcent * HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+        pygame.draw.rect(SCREEN, RED, hp_rect)
 
 font = pygame.font.SysFont(None, 48)
 
@@ -24,18 +174,13 @@ font = pygame.font.SysFont(None, 48)
 clock = pygame.time.Clock()
 
 # Create player variables
-#player_size = random.randint(5, 50)
-# player_size = 50
-# player_x = c.WIDTH // 2 - player_size // 2
-# player_y = c.HEIGHT - player_size - 10
-# player_speed = 350
-# right_edge = c.WIDTH - player_size + 1
-# bottom_edge = c.HEIGHT - player_size + 0
-# player_rect = None
-player = p.Player()
+player = Player()
 
 game_not_started = True
 time_has_failed = 0
+
+# create the hud
+hud = Hud()
 
 #add more power ups examples:
 #(smaller player, smaller obstacle/faster,obstacle bigger,)
@@ -44,7 +189,7 @@ time_has_failed = 0
 
 # Creating powerup variables
 power_up_speed = 300
-power_up_color = (c.BLUE)
+power_up_color = (BLUE)
 shield_timer = 0
 slow_down_timer = 0
 power_ups = []
@@ -56,58 +201,11 @@ obstacle_speed = 500
 obstacles = []
 obstacle_spawn_timer = 0
 obstacle_spawn_delay = 0.5
-obstacle_x = random.randint(0, c.WIDTH - obstacle_width)
+obstacle_x = random.randint(0, WIDTH - obstacle_width)
 obstacle_y = -obstacle_height
 obstacle_spawn_delay = 0.5
 cheating = False
 postCheat = False
-
-
-# def player_update():
-#     global player_x
-#     global player_y
-
-#     # move the player
-#     keys = pygame.key.get_pressed()
-#     move_x = 0
-#     move_y = 0
-#     if keys[pygame.K_UP]:
-#         move_y -= 1
-#     if keys[pygame.K_DOWN]:
-#         move_y += 1
-#     if keys[pygame.K_LEFT]:
-#         move_x -= 1
-#     if keys[pygame.K_RIGHT]:
-#         move_x += 1
-#     player_move = pygame.math.Vector2(move_x, move_y)
-#     # correct diagonal movement
-#     if player_move.length() > 0:
-#         player_move = player_move.normalize() * player_speed * frame_time
-
-#     # Moving player left/right
-#     player_x += player_move.x
-#     player_y += player_move.y
-
-#     # Keeps player in the screen
-
-#     # If player is partialy or totaly off screen on the left
-#     if player_x < 0:
-#         player_x = 0
-
-#     # If player is partialy or totaly off screen on the right
-#     elif player_x > right_edge:
-#         player_x = right_edge
-
-#     # If player is partialy or totaly off screen on the bottom
-#     if player_y > bottom_edge:
-#         player_y = bottom_edge
-
-
-# def player_draw():
-#     global player_rect
-#     player_rect = pygame.Rect(player_x, player_y, player_size, player_size)
-#     pygame.draw.rect(c.SCREEN, c.BLUE, player_rect)
-
 
 def obstacle_update():
     pass
@@ -117,10 +215,25 @@ def obstacle_draw(obstacle):
     obstacle['rect'] = pygame.Rect(obstacle['x'], obstacle['y'],
                                    obstacle['width'], obstacle['height'])
     if obstacle['type'] == 2:
-        pygame.draw.rect(c.SCREEN, c.ORANGE, obstacle['rect'])
+        pygame.draw.rect(SCREEN, ORANGE, obstacle['rect'])
     else:
-        pygame.draw.rect(c.SCREEN, c.RED, obstacle['rect'])
+        pygame.draw.rect(SCREEN, RED, obstacle['rect'])
 
+def cheating_text_draw():
+    # draw cheating text 1
+    text = font.render("YOU'RE A BAD PERSON ", True, RED)
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    SCREEN.blit(text, text_rect)
+
+    # draw cheating text 2
+    text2 = font.render("CHEATER!!!!!!! ", True, RED)
+    text_rect = text2.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 40))
+    SCREEN.blit(text2, text_rect)
+
+def payback_text_draw():
+        text = font.render("THIS IS PAYBACK", True, RED)
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 40))
+        SCREEN.blit(text, text_rect)
 
 while game_not_started:
     keys = pygame.key.get_pressed()
@@ -130,10 +243,10 @@ while game_not_started:
     if keys[pygame.K_RSHIFT]:
         game_not_started = False
         print("Yay")
-    c.SCREEN.fill(c.GREEN)
-    text = font.render("Press shift on the right to begin", True, c.YELLOW)
-    text_rect = text.get_rect(center=(c.WIDTH // 2, c.HEIGHT // 2))
-    c.SCREEN.blit(text, text_rect)
+    SCREEN.fill(GREEN)
+    text = font.render("Press shift on the right to begin", True, YELLOW)
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    SCREEN.blit(text, text_rect)
     pygame.display.flip()
 
 lives = 7
@@ -152,15 +265,17 @@ while running:
     slow_down_timer -= frame_time
     time_has_failed += frame_time
     #print(time_has_failed)
-    text = font.render(str(lives), True, c.RED)
+    text = font.render(str(lives), True, RED)
     text_rect = text.get_rect(center=(40, 40))
-    c.SCREEN.blit(text, text_rect)
+    SCREEN.blit(text, text_rect)
 
     ##################
     # part 1: update #
     ##################
 
     player.update(frame_time)
+
+    hud.update(frame_time)
 
     # Speeds up obstacles, player and powerups
     if time.time() > start_time + 10:
@@ -247,7 +362,7 @@ while running:
         else:
             # move the obstacles normal speed if no powerup
             obstacle['y'] += obstacle['speed'] * frame_time
-        if obstacle['y'] > c.HEIGHT:
+        if obstacle['y'] > HEIGHT:
             if obstacle["type"] == 3 or obstacle["type"] == 4:
                 new_obstacle = {
                     # making sure the obstacle could hit the player by spawning within the horizontal boudaries
@@ -313,11 +428,14 @@ while running:
 
     for power_up in power_ups:
         power_up['y'] += power_up_speed * frame_time
-        if power_up['y'] > c.HEIGHT:
+        if power_up['y'] > HEIGHT:
             power_ups.remove(power_up)
 
-    # Start drawing
-    c.SCREEN.fill(c.WHITE)
+    # draw the backround
+    SCREEN.fill(WHITE)
+    hud.draw()
+
+    # TODO: move this to player update
     if player.get_pos().y < 0:
         if not cheating:
             cheatTime = time.time()
@@ -330,19 +448,11 @@ while running:
                 postCheat = True
                 postCheatTime = time.time()
 
-        text = font.render("YOU'RE A BAD PERSON ", True, c.RED)
-        text_rect = text.get_rect(center=(c.WIDTH // 2, c.HEIGHT // 2))
-        c.SCREEN.blit(text, text_rect)
-
-        text2 = font.render("CHEATER!!!!!!! ", True, c.RED)
-        text_rect = text2.get_rect(center=(c.WIDTH // 2, c.HEIGHT // 2 + 40))
-        c.SCREEN.blit(text2, text_rect)
+        cheating_text_draw()
 
     if postCheat:
         if time.time() < postCheatTime + 2:
-            text = font.render("THIS IS PAYBACK", True, c.RED)
-            text_rect = text.get_rect(center=(c.WIDTH // 2, c.HEIGHT // 2 - 40))
-            c.SCREEN.blit(text, text_rect)
+            payback_text_draw()
         else:
             postCheat = False
 
@@ -352,21 +462,22 @@ while running:
     for obstacle in obstacles[:]:
         obstacle_draw(obstacle)
 
+        # checking for collision with player
         if player._rect.colliderect(obstacle['rect']):
             print("Collision detected!")
             if shield_timer > 0:
                 obstacles.remove(obstacle)
             else:
                 obstacles.remove(obstacle)
-                lives -= 1
+                player.take_damage(OBSTACLE_DMG)
 
     for power_up in power_ups:
         power_up_rect = pygame.Rect(power_up['x'], power_up['y'],
                                     power_up['width'], power_up['height'])
         if power_up['type'] == 1:
-            pygame.draw.rect(c.SCREEN, c.BLUE, power_up_rect)
+            pygame.draw.rect(SCREEN, BLUE, power_up_rect)
         else:
-            pygame.draw.rect(c.SCREEN, c.YELLOW, power_up_rect)
+            pygame.draw.rect(SCREEN, YELLOW, power_up_rect)
 
         if player._rect.colliderect(power_up_rect):
             if power_up['type'] == 1:
@@ -376,21 +487,21 @@ while running:
             power_ups.remove(power_up)
 
     if shield_timer > 0:
-        text = font.render(str(round(shield_timer)), True, c.RED)
-        text_rect = text.get_rect(center=(c.WIDTH // 2, 20))
-        c.SCREEN.blit(text, text_rect)
+        text = font.render(str(round(shield_timer)), True, RED)
+        text_rect = text.get_rect(center=(WIDTH // 2, 20))
+        SCREEN.blit(text, text_rect)
 
     if slow_down_timer > 0:
-        text = font.render(str(round(slow_down_timer)), True, c.RED)
-        text_rect = text.get_rect(center=(c.WIDTH // 2, 60))
-        c.SCREEN.blit(text, text_rect)
+        text = font.render(str(round(slow_down_timer)), True, RED)
+        text_rect = text.get_rect(center=(WIDTH // 2, 60))
+        SCREEN.blit(text, text_rect)
 
-    if lives <= 0:
+    if player.get_hp() <= 0:
         running = False
-        c.SCREEN.fill(c.BLACK)
-        text = font.render("GAME OVER", True, c.RED)
-        text_rect = text.get_rect(center=(c.WIDTH // 2, c.HEIGHT // 2))
-        c.SCREEN.blit(text, text_rect)
+        SCREEN.fill(BLACK)
+        text = font.render("GAME OVER", True, RED)
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        SCREEN.blit(text, text_rect)
 
     pygame.display.flip()
 time.sleep(5)
