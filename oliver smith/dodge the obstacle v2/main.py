b8a7c1f90045cce2caa00fa6236e48d9d3bbcf3b
HankDeTank05 <henryhankholman@gmail.com>
2025-09-15 20:02:08 -0500

    oliver session 9/15

--- oliver smith/dodge the obstacle v2/main.py
+++ oliver smith/dodge the obstacle v2/main.py
@@ -1,6 +1,6 @@
 # language imports (these are built in to python)
 import random
-import time
+import copy
 
 # library imports (this is stuff that's not ours, but not built in to python)
 import pygame
@@ -51,6 +51,7 @@ HEALTH_BAR_HEIGHT = 50
 
 class Obstacle:
 
+    # constructor
     def __init__(self):
         self._rect = pygame.Rect(
             random.randint(0, WIDTH - OBSTACLE_WIDTH),
@@ -59,17 +60,27 @@ class Obstacle:
             OBSTACLE_HEIGHT
         )
 
+    # game functions
+
     def update(self, frame_time):
         self._rect.move_ip(0, OBSTACLE_SPEED * frame_time)
 
     def draw(self):
         pygame.draw.rect(SCREEN, RED, self._rect)
 
+    # accessors
+
+    def get_rect(self) -> pygame.Rect:
+        return self._rect
+
 class ObstacleManager:
 
+    # constructor
     def __init__(self):
         self._obses: list[Obstacle] = []
         self._spawn_timer = 0
+
+    # game functions
 
     def update(self, frame_time):
         self._spawn_timer += frame_time
@@ -82,6 +93,18 @@ class ObstacleManager:
     def draw(self):
         for obs in self._obses:
             obs.draw()
+
+    # accessors
+
+    def get_obses(self) -> list[Obstacle]:
+        return copy.deepcopy(self._obses)
+    
+    # mutators
+
+    def remove_obses(self, obses_indices: list[int]):
+        obses_indices.sort(reverse=True)
+        for obs_index in obses_indices:
+            self._obses.pop(obs_index)
 
 class Player:
 
@@ -143,7 +166,7 @@ class Player:
     def draw(self):
         pygame.draw.rect(SCREEN, BLUE, self._rect)
 
-    # getters
+    # accessors
 
     def get_pos(self) -> pygame.math.Vector2:
         return pygame.math.Vector2(self._rect.topleft)
@@ -151,7 +174,10 @@ class Player:
     def get_hp(self) -> int:
         return self._hp
 
-    # setters
+    def get_rect(self) -> pygame.Rect:
+        return self._rect
+
+    # mutators
 
     def set_pos(self, new_pos: pygame.math.Vector2):
         self._rect.update(new_pos, self._rect.size)
@@ -173,9 +199,12 @@ class Player:
 
 class Hud:
 
+    # constructor
     def __init__(self):
         self._max_health_bar_width = HEALTH_BAR_WIDTH
         self._health_pcent = 1
+
+    # game functions
 
     def update(self, frame_time):
         self._health_pcent = player.get_hp() / PLAYER_MAX_HEALTH
@@ -222,6 +251,18 @@ while running:
     player.update(frame_time)
     obs_man.update(frame_time)
     hud.update(frame_time)
+
+    # do collision
+    collided_indices = []
+    obses_list = obs_man.get_obses()
+    for obs_index in range(len(obses_list)):
+        obstacle = obses_list[obs_index]
+        player_rect = player.get_rect()
+        obs_rect = obstacle.get_rect()
+        if player_rect.colliderect(obs_rect):
+            player.take_damage(OBSTACLE_DMG)
+            collided_indices.append(obs_index)
+    obs_man.remove_obses(collided_indices)
 
     ################
     # PART 2: DRAW #
