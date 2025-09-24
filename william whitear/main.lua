1818153b0ef4f379f4eb52768a3cd9a5e6002804
HankDeTank05 <henryhankholman@gmail.com>
2025-09-24 18:01:46 -0500

    will session 9/24

--- william whitear/main.lua
+++ william whitear/main.lua
@@ -1,37 +1,67 @@
 function love.load()
-    x = 0 
-    y = 0
-    width = 30
-    height = 100
-    speed = 25
+    player = {
+        x = 0,
+        y = 0,
+        width = 30,
+        height = 50,
+        speed = 700,
+    }
+
+    platform = {
+        x = 0 ,
+        y = 100 ,
+        width = 70,
+        height = 20
+    }
+
+    gravity = 10
 
 end
 
 function love.update(frame_time)
+    position_delta = player.speed * frame_time
+
     -- move right
     if love.keyboard.isDown('d') then
-        x = x + speed
+        player.x = player.x + position_delta
     end
+    
     --move left
     if love.keyboard.isDown('a') then
-        x = x + -speed
-    end
-    -- move down
-    if love.keyboard.isDown('s') then
-        y = y + speed
-    end
-    -- move up
-    if love.keyboard.isDown("w") then
-        y = y + -speed
+        player.x = player.x + -position_delta
     end
 
-    -- prevent the rectangle from going off-screen on the top edge
-    if y < 0 then
-        -- TODO: next time!
+    -- move down
+    if love.keyboard.isDown('s') then
+        player.y = player.y + position_delta
     end
+
+    -- move up
+    if love.keyboard.isDown("w") then
+        player.y = player.y + -position_delta
+    end
+
+    --  apply gravity
+    player.y = player.y + gravity
+
+    -- prevent the rectangle from going off-screen on the left and right edge
+    if player.x < 0 then
+        player.x = 0
+    elseif player.x + player.width - 1 > love.graphics.getWidth() then
+        player.x = love.graphics.getWidth() - player.width + 1
+    end
+
+    -- prevent the rectangle from going off-screen on the top and bottom edge
+    if player.y < 0 then 
+        player.y = 0
+    elseif player.y + player.height - 1 > love.graphics.getHeight() then
+        player.y = love.graphics.getHeight() - player.height + 1
+    end
+
 end
 
 function love.draw()
-    love.graphics.rectangle('fill', x, y, width, height)
+    love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
+    love.graphics.rectangle('fill', platform.x , platform.y , platform.width , platform.height)
     love.graphics.print("Hello World", 400, 300)
 end
