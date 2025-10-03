01850fd88372f31488bf08525c5c5c556bc4a81c
HankDeTank05 <henryhankholman@gmail.com>
2025-10-03 17:01:10 -0500

    owen session 10/3

--- owen guttosch/block.py
+++ owen guttosch/block.py
@@ -7,7 +7,7 @@ class Block:
         self.x: int = grid_x
         self.y: int = grid_y
         self.color:      tuple =color
-        self.rect: Rect = Rect((self.x, self.y), ())
+        self.rect: Rect = Rect((self.x, self.y), (10, 10))
         '''#shift-3'''
 
     def __repr__(self):
@@ -17,4 +17,23 @@ class Block:
         pass
 
     def draw(self):
-        pass
+        screen.draw.filled_rect(self.rect, self.color)
+        screen.draw.rect(self.rect, (238, 230, 0))
+
+'''    
+WIDTH = 360
+HEIGHT = 240
+
+my_block = Block(grid_x=7, grid_y=12, color=(15, 72, 94))
+print(my_block.x)
+print(my_block.y)
+print(my_block.color)
+
+def update():
+    my_block.update()
+
+def draw():
+    my_block.draw()
+
+pgzrun.go()
+'''
--- owen guttosch/tetromino.py
+++ owen guttosch/tetromino.py
@@ -1,5 +1,39 @@
+import pgzrun
+
+'''imports
+the
+class
+'''
+from block import Block
+
 class Tetromino:
 
     # constructor
-    def __init__(self):
-        pass
+    def __init__(self, blocks4: list[Block]):
+        self.blocks4: list[Block] = blocks4
+
+    def draw_the_thingimagiggie(self):
+        for block in self.blocks4:
+            block.draw()
+
+WIDTH = 360
+HEIGHT = 240
+
+BLOCKCOLORANDSTUFF = (15, 72, 94)
+
+myblocks = [
+    Block(7, 20, BLOCKCOLORANDSTUFF),
+    Block(6, 20, BLOCKCOLORANDSTUFF),
+    Block(6, 21, BLOCKCOLORANDSTUFF),
+    Block(6, 22, BLOCKCOLORANDSTUFF)
+]
+my_tetro = Tetromino(myblocks)
+
+def update():
+    pass
+
+def draw():
+    my_tetro.draw_the_thingimagiggie()
+
+pgzrun.go()
+
