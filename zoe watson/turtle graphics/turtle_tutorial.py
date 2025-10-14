fbbc6f9b5034b5d44978d79173ac47738d249e7d
HankDeTank05 <henryhankholman@gmail.com>
2025-10-14 16:35:05 -0500

    zoe session 10/14

--- zoe watson/turtle graphics/turtle_tutorial.py
+++ zoe watson/turtle graphics/turtle_tutorial.py
@@ -7,13 +7,24 @@ def colors_of_ROYGBV(colors):
             forward(steps)
             right(30)
 
-def star(line_color, fill_color):
+def star(line_color, fill_color, angle):
     color(line_color)
     fillcolor(fill_color)
+    start_x = round(xcor(), 3)
+    start_y = round(ycor(), 3)
+    print(f"started at {start_x},{start_y}")
     begin_fill()
     while True:
         forward(200)
-        left(100)
-        if abs (pos()) < 1:
+        left(angle)
+        if round(xcor(), 3) == start_x and round(ycor(), 3) == start_y:
             break
     end_fill()
+
+def stars(star_count):
+    forward(200 * ((star_count/2)-1))
+    for s in range(star_count):
+        star('black', 'indigo', 144)
+        backward(200)
+
+reset()
