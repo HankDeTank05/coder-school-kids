1c5de11bacadd5797c7d0821ef0b42fbc4f50e06
HankDeTank05 <henryhankholman@gmail.com>
2025-10-20 17:03:01 -0500

    cash session 10/20

---
+++ cash kadri/java/basics/Basics.java
@@ -0,0 +1,32 @@
+class Basics {
+    private static void basics1(){
+        String newSentence = "I have a good coder teacher named Henry";
+        int age = 15;
+        int birth = 2015;
+        String sentence = "My name is AI nice to meet you"; 
+        // print(Yee)
+        System.out.println(sentence);
+        System.out.println(newSentence);
+        if(age>=16){
+            System.out.println("I can drive now because I am " + age);
+        }
+        else if(age == 15){
+            System.out.println("I can't drive without my parent but i can drive");
+        }
+        else if(age==14){
+            System.out.println("I can't drive at all but next year I can");
+        }
+        else{
+            System.out.println("I can't drive AT ALL.");
+        }
+        System.out.println("THAT'S A WROOP");
+    }
+    public static void main(String[] args) {
+        Basics.basics1();
+        String[] pokemon = {"Diggersby", "Greninja", "Victreebel"};
+        for(int index = 0; index < pokemon.length; index+=1){
+            System.out.println(pokemon[index]);
+        }
+        System.out.println(pokemon);
+    }
+}
