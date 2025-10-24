cee43f4879829b4853bc92b08d34a79be7add70e
HankDeTank05 <henryhankholman@gmail.com>
2025-10-24 18:05:45 -0500

    zarna session 10/24

--- zarna modi/maze.py
+++ zarna modi/maze.py
@@ -1,8 +1,15 @@
 wall = "#"
 space = "-"
 
+def char_to_cell(char_i: int) -> int:
+    assert(char_i % 2 == 1)
+    return (char_i - 1) // 2
+
+def cell_to_char(cell_i: int) -> int:
+    return cell_i * 2 + 1
+
 cell_size = 10
-maze_size = cell_size * 2 + 1
+maze_size = cell_to_char(cell_size)
 
 maze = []
 
@@ -11,17 +18,33 @@ for y in range(maze_size):
 
     # populate the row with wall characters
     for x in range(maze_size):
-        maze[y].append(wall)
+        if x%2 == 1 and y%2 == 1:
+            maze[y].append(space)
+        else:
+            maze[y].append(wall)
+        print(maze[y][x], end=" ")
     
-    # print the completed row
-    print(maze[y])
+    print()
 
-# print(maze)
+visited_data = []
 
+for y in range(cell_size):
+    visited_data.append([]) # add an empty row to the visited data grid
 
-def maze_gen_recursive():
-    # 1. Choose a starting cells
+    # populate the row with "false" (which means it's not visited)
+    for x in range(cell_size):
+        visited_data[y].append(False)
+        print(visited_data[y][x], end=" ")
+
+    print()
+
+# 1. Choose a starting cell
+def maze_gen_recursive(x: int, y: int):
+    # assert() has to be true or it crashes
+    assert(x % 2 == 1) # checking if x is odd (will crash if not)
+    assert(y % 2 == 1) # checking if y is odd (will crash if not)
     # 2. Mark the current cell as visited
+
     # 3. While the current cell has any unvisited neighbor cells
         # 3.1 Choose one of the unvisited neighbors
         # 3.2 Remove the wall between the current cell and the chosen cell
