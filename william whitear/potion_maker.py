3303ba4344fcca2d18d7d54c49971fd7dd435ee2
HankDeTank05 <henryhankholman@gmail.com>
2025-11-05 17:58:59 -0600

    will w session 11/5

--- william whitear/potion_maker.py
+++ william whitear/potion_maker.py
@@ -4,7 +4,7 @@ import pygame
 SCREEN_WIDTH = 1280 
 SCREEN_HEIGHT = 720
 FPS = 100
-
+selected = 1
 # game constants
 MIN_ACID = 1 
 MAX_ACID = 5
@@ -51,6 +51,44 @@ lt_arow_prev_state = False
 rt_arow_prev_state = False
 rt_arow_curr_state = False
 
+def prev_category():
+    global category_index
+    category_index -= 1
+    if category_index < 0:
+        category_index = len(ITEM_X) - 1
+
+def next_category():
+    global category_index
+    category_index += 1 
+    if category_index >= len(ITEM_X):
+        category_index = 0
+
+
+def prev_element():
+    global current_element_index
+    current_element_index -= 1
+    if current_element_index == -1:  
+        current_element_index += 3
+
+def next_element():
+    global current_element_index
+    current_element_index += 1 # it adds one to the variable
+    if current_element_index == 3: # if the index variable is too big, then....
+        current_element_index -= 3 # decrease the index variable back to zero
+
+
+def increase_acid():
+    global current_acid
+    current_acid += 1 # increses the amont of acid in the potion
+    if current_acid > MAX_ACID: # if there is too much acid in the potion...
+        current_acid = MIN_ACID # makes the least amoont of acid in the potion
+
+def decrease_acid():
+    global current_acid
+    current_acid -= 1
+    if current_acid < MIN_ACID:
+        current_acid = MAX_ACID
+
 while running: 
     #allows the player to quit the game
     for event in pygame.event.get():
@@ -66,26 +104,24 @@ while running:
     # read for up and down arrow key presses here
     up_arow_curr_state = keys[pygame.K_UP]
     if up_arow_curr_state == True and up_arow_prev_state == False: # it cheaks if you are pressing the up arrow #
-        current_element_index += 1 # it adds one to the variable
-        if current_element_index == 3: # if the index variable is too big, then....
-            current_element_index -= 3 # decrease the index variable back to zero
+        if category_index == 0: # cheak if you should modify the element
+            next_element()
+        elif category_index == 1: # otherwise cheak if you should modify the acid
+            increase_acid()
     dn_arow_curr_state =  keys[pygame.K_DOWN]
     if dn_arow_curr_state == True and dn_arow_prev_state == False:
-        current_element_index -= 1
-        if current_element_index == -1:  
-            current_element_index += 3
+        if category_index == 0:
+            prev_element()
+        elif category_index == 1:
+            decrease_acid()
 
-    # TODO: next time, read for left and right arrow key presses here
+    # read for left and right arrow key presses here
     lt_arow_curr_state = keys[pygame.K_LEFT]
     if lt_arow_curr_state == True and lt_arow_prev_state == False:
-        category_index -= 1
-        if category_index < 0:
-            category_index = len(ITEM_X) - 1
+        prev_category()
     rt_arow_curr_state = keys[pygame.K_RIGHT]
     if rt_arow_curr_state == True and rt_arow_prev_state == False:
-        category_index += 1 
-        if category_index >= len(ITEM_X):
-            category_index = 0
+        next_category()
 
     # render element text 
     element_text = font.render(elements_text[current_element_index], True, WHITE)
