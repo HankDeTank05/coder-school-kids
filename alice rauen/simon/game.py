60d8986f773205a5f38f0d6b03701b87375bb0c3
HankDeTank05 <henryhankholman@gmail.com>
2025-11-09 13:03:25 -0600

    alice session 11/9

--- alice rauen/simon/game.py
+++ alice rauen/simon/game.py
@@ -1,5 +1,3 @@
-# TO HENRY: Implement click checking logic, increment seq_index on clicks, etc
-
 import pgzrun
 
 import random
@@ -15,6 +13,7 @@ CHICKEN_BANANA=Actor('chicken_banana!')
 CHICKEN_BANANA.center=(320,240)
 
 LEVEL_UP=Actor('levelup')
+LEVEL_UP.center=(320,240)
 # colors go here
 lavender=(181,126,220)
 columbia=(155,221,255)
@@ -43,11 +42,14 @@ orange_color=ruby
 # sequence
 colors=['purple', 'blue', 'red', 'orange']
 my_sequence=['purple']
-number=0000
 click_counter=0
 is_clicked=False
-seq_index = 0
+click_index = 0
+sequence_index=0
+sequence_timer=0
 curent_color=None
+game_state='sequence'
+level_timer=0
 
 # my_sequence.append(random.choice(colors))
 # my_sequence.append(random.choice(colors))
@@ -59,11 +61,19 @@ curent_color=None
 #    time.sleep(3)
 
 def level_up():
-    global seq_index
+    global click_index
+    global game_state
+    global sequence_index
+    global sequence_timer
+    global level_timer
     print("LEVEL UP!")
-    seq_index=0
+    click_index=0
+    sequence_index=0
+    sequence_timer=0 
     new_color=random.choice(colors)
     my_sequence.append(new_color)
+    game_state='level up'
+    level_timer=0
 
 
 def on_mouse_down(pos):
@@ -72,35 +82,40 @@ def on_mouse_down(pos):
     global red_color
     global orange_color
     global is_clicked
-    global seq_index
+    global click_index
     print(pos)
     # is_clicked=True
     # purple 
-    click=None 
-    if purple.left<pos[0]<purple.right and purple.top<pos[1]<purple.bottom:
-        click=check_rect('purple')
-        purple_color=white
-    elif blue.left<pos[0]<blue.right and blue.top<pos[1]<blue.bottom:
-        click=check_rect('blue')
-        blue_color=white
-    elif orange.left<pos[0]<orange.right and orange.top<pos[1]<orange.bottom:
-        click=check_rect('orange')
-        orange_color=white
-    elif red.left<pos[0]<red.right and red.top<pos[1]<red.bottom:
-        click=check_rect('red')
-        red_color=white
+    if game_state=='play':
+        click=None 
+        if purple.left<pos[0]<purple.right and purple.top<pos[1]<purple.bottom:
+            click=check_rect('purple')
+            purple_color=white
+        elif blue.left<pos[0]<blue.right and blue.top<pos[1]<blue.bottom:
+            click=check_rect('blue')
+            blue_color=white
+        elif orange.left<pos[0]<orange.right and orange.top<pos[1]<orange.bottom:
+            click=check_rect('orange')
+            orange_color=white
+        elif red.left<pos[0]<red.right and red.top<pos[1]<red.bottom:
+            click=check_rect('red')
+            red_color=white
 
-    if click ==True:
-        print("CORRECT")
-        seq_index+=1
-        if seq_index ==len(my_sequence):
-            level_up()
+        if click ==True:
+            print("CORRECT")
+            click_index+=1
+            if click_index==len(my_sequence):
+                level_up()
+
+
+        elif click==False:
+            pass
 
 
 def on_mouse_up(pos):
     global purple_color,blue_color,orange_color,red_color
     if purple_color ==white:
-         purple_color=lavender
+        purple_color=lavender
     if blue_color ==white:
         blue_color=columbia
     if red_color ==white:
@@ -109,7 +124,8 @@ def on_mouse_up(pos):
         orange_color=ruby
 
 def update():
-    global number
+    global sequence_index
+    global sequence_timer
     global curent_color
     global purple_color
     global blue_color
@@ -117,40 +133,48 @@ def update():
     global orange_color
     global click_counter
     global is_clicked
-    if number<len(my_sequence):
-        curent_color=my_sequence[number]
-    else:
-        curent_color=None
-    # if is_clicked:
-    #     click_counter+=1
-    # if click_counter>=15:
-    #     purple_color=lavender
-    #     blue_color=columbia
-    #     red_color=corel
-    #     orange_color=ruby
-    #     click_counter=0
+    global game_state
+    global level_timer
+    if game_state=='sequence':
+        if sequence_index<len(my_sequence):
+            curent_color=my_sequence[sequence_index]
+            sequence_timer+=1
+            if sequence_timer>=60:
+                sequence_timer=0
+                sequence_index+=1
+                if sequence_index==len(my_sequence):
+                    game_state='play'
+                    curent_color=None 
+        else:
+            curent_color=None
+    elif game_state=='level up':
+        level_timer+=1
+        if level_timer>=180:
+            game_state='sequence'
 
 def draw():
+    screen.clear()
     screen.draw.filled_rect(purple,purple_color)
     screen.draw.filled_rect(blue,blue_color)
     screen.draw.filled_rect(red,red_color)
     screen.draw.filled_rect(orange,orange_color)
     #determine where the chicken banana is going to go
     
-    if curent_color=='blue':
-        CHICKEN_BANANA.center=blue.center
-        CHICKEN_BANANA.draw()
-    elif curent_color=='purple':
-        CHICKEN_BANANA.center=purple.center
-        CHICKEN_BANANA.draw()
-    elif curent_color=='orange':    
-        CHICKEN_BANANA.center=orange.center
-        CHICKEN_BANANA.draw()
-    elif curent_color=='red':
-        CHICKEN_BANANA.center=red.center
-        CHICKEN_BANANA.draw()
-    elif curent_color == None:
-        pass
+    if game_state=='sequence':
+        if curent_color=='blue':
+            CHICKEN_BANANA.center=blue.center
+            CHICKEN_BANANA.draw()
+        elif curent_color=='purple':
+            CHICKEN_BANANA.center=purple.center
+            CHICKEN_BANANA.draw()
+        elif curent_color=='orange':    
+            CHICKEN_BANANA.center=orange.center
+            CHICKEN_BANANA.draw()
+        elif curent_color=='red':
+            CHICKEN_BANANA.center=red.center
+            CHICKEN_BANANA.draw()
+    elif game_state=='level up':
+        LEVEL_UP.draw()
 
 def check_rect(color: str) -> bool:
     print(f"This ran and color: {color} and getnext: {get_next()}")
@@ -159,7 +183,7 @@ def check_rect(color: str) -> bool:
     return False
 
 def get_next() -> str:
-    return my_sequence[seq_index]
+    return my_sequence[click_index]
 
 pgzrun.go()
  
