a729e9b577246f7efe24b709cd08e7f9cd880a1e
HankDeTank05 <henryhankholman@gmail.com>
2025-11-18 18:33:42 -0600

    ross session 11/18

--- Ross epstein/main.py
+++ Ross epstein/main.py
@@ -1,12 +1,13 @@
 from interact import moral_choice, prompt_for_input
 from player import Player
+from scenario_data import game_graph
 
 player = Player()
 
-moral_choice("test", "evil test", "neutral test", "good test")
+# moral_choice("test", "evil test", "neutral test", "good test")
 
-prompt_for_input("well exploring the forest you find a portal do you want to enter.",
-                 ["yes","no"])
+# prompt_for_input("well exploring the forest you find a portal do you want to enter.",
+#                  ["yes","no"])
 
 
 
