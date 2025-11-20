70b75e19990f55cafeb3ac58bcf4685e091754d7
HankDeTank05 <henryhankholman@gmail.com>
2025-11-19 18:01:59 -0600

    will w session 11/19

--- william whitear/potion maker/potion_maker.py
+++ william whitear/potion maker/potion_maker.py
@@ -22,6 +22,8 @@ for item_index in range(ITEMS_ON_SCREEN):
     x = (SCREEN_WIDTH / (ITEMS_ON_SCREEN + 1)) * (item_index + 1)
     ITEM_X.append(x)
 
+SPRITE_SCALE = 7
+
 HIGHLIGHT_RECT_WIDTH = 100
 HIGHLIGHT_RECT_HEIGHT = 50
 
@@ -33,18 +35,32 @@ FOREST_GREEN = pygame.Color(34,139,34)
 DARK_VIOLET = pygame.Color(148,0,211)
 
 #sprites
-if IN_DEVELOPMENT: # TODO: explain this at the start of next session (delete this once explained!)
-    bottle_classic_rel_path = os.path.join("william whitear", "potion maker", "assets", "images", "bottle_classic.png")
-    # bottle_cube_rel_path = 
-else:
-    bottle_classic_rel_path = os.path.join("assets", "images", "bottle_classic.png")
-    # bottle_cube_rel_path = 
+# if IN_DEVELOPMENT: # TODO: explain this at the start of next session (delete this once explained!)
+#     bottle_classic_rel_path = os.path.join("william whitear", "potion maker", "assets", "images", "bottle_classic.png")
+#     bottle_cube_rel_path = os.path.join("william whitear", "potion maker", "assets", "images", "bottle_cube.png")
+# else:
+#     bottle_classic_rel_path = os.path.join("assets", "images", "bottle_classic.png")
+#     bottle_cube_rel_path = os.path.join("assets", "images", "bottle_cube.png")
 
-bottle_classic_abs_path = os.path.abspath(bottle_classic_rel_path)
-spr_bottle_classic = pygame.image.load(bottle_classic_abs_path)
+# bottle_classic_abs_path = os.path.abspath(bottle_classic_rel_path)
+# spr_bottle_classic = pygame.image.load(bottle_classic_abs_path)
+# spr_bottle_classic = pygame.transform.scale(spr_bottle_classic, (SPRITE_SCALE*spr_bottle_classic.get_width(), SPRITE_SCALE*spr_bottle_classic.get_height()))
 
-# bottle_cube_abs_path = 
-# spr_bottle_cube = 
+# bottle_cube_abs_path = os.path.abspath(bottle_cube_rel_path)
+# spr_bottle_cube = pygame.image.load(bottle_cube_abs_path)
+# spr_bottle_cube = pygame.transform.scale(spr_bottle_cube, (SPRITE_SCALE*spr_bottle_cube.get_width(), SPRITE_SCALE*spr_bottle_cube.get_height()))
+
+def load_scaled_sprite(img_name, scale = 1) -> pygame.Surface:
+    if IN_DEVELOPMENT:
+        rel_path = os.path.join("william whitear", "potion maker", "assets", "images", img_name)
+    else:
+        rel_path = os.path.join("assets", "images", img_name)
+    abs_path =  os.path.abspath(rel_path)
+    spr = pygame.image.load(abs_path)
+    spr = pygame.transform.scale(spr, (scale*spr.get_width(), scale*spr.get_height()))
+    return spr
+
+spr_bottle = load_scaled_sprite (img_name="bottle_cube.png", scale= SPRITE_SCALE)
 
 #pygame setup
 pygame.init()
@@ -61,6 +77,9 @@ elements_text = ["ice", "earth", "magic"]
 current_element_index = 0
 
 current_acid = MIN_ACID
+
+bottle_draw_pos = pygame.math.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT * (2/3))
+spr_center_adjust = pygame.math.Vector2(-spr_bottle.get_width()/ 2, - spr_bottle.get_height()/ 2)
 
 
 # key states 
@@ -177,6 +196,8 @@ while running:
 
     # this code is rendering the highlight rectangle on screen
     pygame.draw.rect(screen, WHITE, hlight_rect, 10)
+
+    screen.blit(spr_bottle, bottle_draw_pos + spr_center_adjust)
 
     # this code makes everything we've drawn appear on screen
     pygame.display.flip()
