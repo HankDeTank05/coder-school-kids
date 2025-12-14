5ff5d796d4a311cf96f19eb394d882207d5b66cc
HankDeTank05 <henryhankholman@gmail.com>
2025-12-14 12:59:34 -0600

    alice session 12/14

--- alice rauen/flappy dunk/game.py
+++ alice rauen/flappy dunk/game.py
@@ -2,18 +2,23 @@ import pgzrun
 
 WIDTH=1920
 HEIGHT=1080
-
-
-
+ 
 WIDTH = 1280
 HEIGHT = 720
 
 #chicken banana
-FLAPPY=Actor('flappy')
+flappy=Actor('flappy')
+flappy.pos = 100, 358
+
+def on_key_down(key):
+    if key==UP:
+        pass
 
 def update():
     pass
 
 def draw():
-    pass
+    screen.clear()
+    flappy.draw()
 
+pgzrun.go()
