adcebcd6e9e736b6b6432264771e439f9abdeacf
HankDeTank05 <henryhankholman@gmail.com>
2026-01-18 13:01:35 -0600

    alice session 1/18

--- alice rauen/flappy dunk/game.py
+++ alice rauen/flappy dunk/game.py
@@ -13,8 +13,10 @@ go_down=False
 go_up=False
 ball_width=160
 
-ring=Actor("ring")
-ring.pos = 667, 467
+ring_back=Actor("pigai123back")
+ring_back.pos = 667, 467
+ring_front=Actor("lionabz456frontsixseven")
+ring_front.pos=667,563
 hoop_width=239
 rim_width=73
 
@@ -35,21 +37,23 @@ def on_key_up(key):
     if key==key.DOWN:
         go_down=False
     
-    if key==key.UP:
-        go_up=False
+    # if key==key.UP:
 
 def update():
+    global go_up
     
     flappy.y+=1
     if go_up==True:
-        flappy.y-=2
-    ring.x-=1
-    if ring.right<0:
-        ring.left=WIDTH
+        go_up=False
+        flappy.y-=67
+    ring_back.x-=1
+    ring_front.x-=1
+    if ring_back.right<0:
+        ring_back.left=WIDTH
 
-    if hoop_width/2-ball_width/2>=abs(flappy.center[0]-ring.center[0]):
+    if hoop_width/2-ball_width/2>=abs(flappy.center[0]-ring_back.center[0]):
         print('swish')
-    elif (hoop_width/2-ball_width/2)+rim_width>=abs(flappy.center[0]-ring.center[0]):
+    elif (hoop_width/2-ball_width/2)+rim_width>=abs(flappy.center[0]-ring_back.center[0]):
         print('basket')
     else:
         print('mrs')
@@ -60,7 +64,8 @@ def update():
 
 def draw():
     screen.clear()
-    ring.draw()
+    ring_back.draw()
     flappy.draw()
+    ring_front.draw()
 
 pgzrun.go()
