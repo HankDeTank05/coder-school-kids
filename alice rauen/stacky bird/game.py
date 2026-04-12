da71bccac5a6e6983d02d4f364af9a911ed7b511
HankDeTank05 <henryhankholman@gmail.com>
2026-04-12 14:05:10 -0500

    alice session 4/12

--- alice rauen/stacky bird/game.py
+++ alice rauen/stacky bird/game.py
@@ -21,13 +21,14 @@ bird_rect=Rect(0, FLORE_HEIGHT-BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
 block_list=[]
 
 OBS_TIME=3
-OBS_MAX_HEIGHT=9 #number of blocks tall
+OBS_MAX_BLOCKS = 9 #number of blocks tall
 time_since_obs = 0
+obs_list=[]
 
 def spawn_obs():
-    obs_height = random.randint(1,OBS_MAX_HEIGHT)
+    obs_height = random.randint(1,OBS_MAX_BLOCKS)
     obs_width = obs_height
-    obs_rect=Rect(270,FLORE_HEIGHT,BLOCK_SIZE,BLOCK_SIZE)
+    obs_rect=Rect(270, FLORE_HEIGHT-BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
     return obs_rect
 
 
@@ -45,6 +46,26 @@ def update(frame_time):
     time_since_obs += frame_time
     if time_since_obs>=OBS_TIME:
         time_since_obs-=OBS_TIME
+        obs_list.append(spawn_obs())
+
+    # move obstacles
+    for obs in obs_list:
+        obs.x-=1
+
+    # check for collision
+    highest_pushed_block_index = len(block_list)
+    for block_index in range(len(block_list)):
+        block = block_list[block_index]
+        for obs in obs_list:
+            if block.colliderect(obs):
+                block.right=obs.left
+                highest_pushed_block_index = min(block_index, highest_pushed_block_index)
+
+
+    # drop the block stack if there's nothing under it
+    for block_index in range(len(block_list)):
+        pass
+
 
 
 def draw():
@@ -52,7 +73,9 @@ def draw():
     screen.draw.filled_rect(sky_rect,COLOR_SKY_BLUE)
     screen.draw.filled_rect(flore_rect, COLOR_BROWN)
     screen.draw.filled_rect(bird_rect, COLOR_BLUE)
-    screen.draw.filled_rect(spawn_obs(),COLOR_PURPLE)
+    #screen.draw.filled_rect(spawn_obs(),COLOR_PURPLE)
+    for obs in obs_list:
+        screen.draw.filled_rect(obs,COLOR_PURPLE)
 
     for block in block_list:
         screen.draw.rect(block,COLOR_DODGER_BLUE)
