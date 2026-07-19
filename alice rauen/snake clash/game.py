1b6cc996a3e17298b39e245286e72824fd51c0db
HankDeTank05 <henryhankholman@gmail.com>
2026-07-19 14:01:55 -0500

    alice session 7/19

---
+++ alice rauen/snake clash/game.py
@@ -0,0 +1,32 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+# game setup
+player=pygame.Rect(0,0,201,102)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    pygame.draw.rect(screen,'skyblue1', player)
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
