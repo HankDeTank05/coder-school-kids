4ec524c41ea7d71ff00eb6aa80c94cbe69597098
HankDeTank05 <henryhankholman@gmail.com>
2026-08-15 11:01:16 -0500

    alice session 8/15

--- alice rauen/snake clash/game.py
+++ alice rauen/snake clash/game.py
@@ -1,18 +1,22 @@
 # Example file showing a basic pygame "game loop"
+import random
+
 import pygame
 
 # pygame setup
+WIDTH=1280
+HEIGHT=720
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
-# game setup
+# SNAKE STUFF
 segments=22
-seg_size = 50
+seg_size = 48 # size of the square
 
 speed=6
-player=[]
+seg_list=[]
 
 moves=[]
 
@@ -20,20 +24,11 @@ colors=[]
 head_color = pygame.Color('skyblue1')
 tail_color = pygame.Color('black')
 for i in range(segments):
-    player.append(pygame.Rect(0,0,seg_size,seg_size))
+    seg_list.append(pygame.Rect(0,0,seg_size,seg_size))
     moves.append(pygame.math.Vector2(0,0))
     colors.append(head_color.lerp(tail_color, i/segments))
 
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # UPDATE THE GAME
-
+def update_snake():
     keys=pygame.key.get_pressed()
     new_move=pygame.math.Vector2(0,0)
     if keys[pygame.K_UP]:
@@ -50,19 +45,69 @@ while running:
         moves.insert(0,new_move)
         old_move=moves.pop()
 
-        # player[0].move_ip(move.x,move.y)
-        for i in range(len(player)):
+        # player[0].move_ip(move.x,move.y) 
+        for i in range(len(seg_list)):
             move=moves[i]*speed
-            segment=player[i]
+            segment=seg_list[i]
             segment.move_ip(move.x,move.y)
+
+            if segment.left<0:
+                segment.left=0
+            if segment.top<0:
+                segment.top=0
+            if segment.bottom>=HEIGHT:
+                segment.bottom=HEIGHT-1
+            if segment.right>=WIDTH:
+                segment.right=WIDTH-1    
+
+def draw_snake():
+    # draw the segments
+    for i in range(len(seg_list)-1, -1, -1):
+        # pygame.draw.rect(screen, colors[i], player[i])
+        pygame.draw.circle(screen,colors[i],seg_list[i].center,seg_list[i].width/2)
+
+    # draw the face
+    face_center = pygame.math.Vector2(seg_list[0].center)
+    pygame.draw.circle(screen,'cyan4', face_center+pygame.math.Vector2(12,-12),5)
+    pygame.draw.circle(screen,'cyan4', face_center+pygame.math.Vector2(-12,-12),5)
+    pygame.draw.arc(screen,'cyan4',seg_list[0].scale_by(0.65),225*(3.14/180),315*(3.14/180),width=2)
+
+# FOOD STUFF
+
+food=[]
+food_size=10 # radius of the circle
+
+def spawn_food():
+    x=random.randint(0,WIDTH-1)
+    y=random.randint(0,HEIGHT-1)
+    food.append(pygame.math.Vector2(x,y))
+
+def draw_food():
+    for center in food:
+        pygame.draw.circle(screen, "mistyrose2", center, food_size)
+
+
+spawn_food()
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # UPDATE THE GAME
+
+    update_snake()
 
     # DRAW THE GAME
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("skyblue")
 
-    for i in range(len(player)-1, -1, -1):
-        pygame.draw.rect(screen, colors[i], player[i])
+    draw_snake()
+    draw_food()
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
