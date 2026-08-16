51bb521eb09816e10e5af4d6cc160c4bb928b1bf
HankDeTank05 <henryhankholman@gmail.com>
2026-08-16 14:01:25 -0500

    alice session 8/16

---
+++ alice rauen/snake clash/common.py
@@ -0,0 +1,21 @@
+import pygame
+
+BACKGROUND_COLOR = pygame.Color('skyblue')
+
+SNAKE_HEAD_COLOR = pygame.Color('orchid4')
+SNAKE_TAIL_COLOR = pygame.Color('slateblue1')
+SNAKE_FACE_COLOR = pygame.Color('orchid')
+
+FOOD_COLORS = [
+    pygame.Color('mistyrose2'),
+    pygame.Color('salmon1'),
+    pygame.Color("chocolate"),
+    pygame.Color('plum'),
+    pygame.Color('peachpuff'),
+    pygame.Color('aliceblue'),
+    pygame.Color('honeydew'),
+    pygame.Color('lime'),
+    pygame.Color('olive'),
+    pygame.Color('orange'),
+    pygame.Color('tomato')
+]
--- alice rauen/snake clash/game.py
+++ alice rauen/snake clash/game.py
@@ -3,6 +3,8 @@ import random
 
 import pygame
 
+from common import *
+
 # pygame setup
 WIDTH=1280
 HEIGHT=720
@@ -21,14 +23,15 @@ seg_list=[]
 moves=[]
 
 colors=[]
-head_color = pygame.Color('skyblue1')
-tail_color = pygame.Color('black')
+head_color = SNAKE_HEAD_COLOR
+tail_color = SNAKE_TAIL_COLOR
 for i in range(segments):
     seg_list.append(pygame.Rect(0,0,seg_size,seg_size))
     moves.append(pygame.math.Vector2(0,0))
     colors.append(head_color.lerp(tail_color, i/segments))
 
 def update_snake():
+    # move the snake
     keys=pygame.key.get_pressed()
     new_move=pygame.math.Vector2(0,0)
     if keys[pygame.K_UP]:
@@ -58,36 +61,51 @@ def update_snake():
             if segment.bottom>=HEIGHT:
                 segment.bottom=HEIGHT-1
             if segment.right>=WIDTH:
-                segment.right=WIDTH-1    
+                segment.right=WIDTH-1
+
+        # check for collision with food    
 
 def draw_snake():
     # draw the segments
     for i in range(len(seg_list)-1, -1, -1):
         # pygame.draw.rect(screen, colors[i], player[i])
-        pygame.draw.circle(screen,colors[i],seg_list[i].center,seg_list[i].width/2)
+        pygame.draw.circle(screen, colors[i],seg_list[i].center,seg_list[i].width/2)
 
     # draw the face
     face_center = pygame.math.Vector2(seg_list[0].center)
-    pygame.draw.circle(screen,'cyan4', face_center+pygame.math.Vector2(12,-12),5)
-    pygame.draw.circle(screen,'cyan4', face_center+pygame.math.Vector2(-12,-12),5)
-    pygame.draw.arc(screen,'cyan4',seg_list[0].scale_by(0.65),225*(3.14/180),315*(3.14/180),width=2)
+    pygame.draw.circle(screen, SNAKE_FACE_COLOR, face_center+pygame.math.Vector2(12,-12),5)
+    pygame.draw.circle(screen, SNAKE_FACE_COLOR, face_center+pygame.math.Vector2(-12,-12),5)
+    pygame.draw.arc(screen, SNAKE_FACE_COLOR, seg_list[0].scale_by(0.65),225*(3.14/180),315*(3.14/180),width=2)
 
 # FOOD STUFF
 
 food=[]
+food_colors=[]
+food_rects=[]
 food_size=10 # radius of the circle
 
 def spawn_food():
-    x=random.randint(0,WIDTH-1)
-    y=random.randint(0,HEIGHT-1)
+    x=random.randint(food_size,WIDTH-1-food_size)
+    y=random.randint(food_size,HEIGHT-1-food_size)
     food.append(pygame.math.Vector2(x,y))
+    food_colors.append(random.choice(FOOD_COLORS))
+    food_rects.append
+
+def spawn_foods(count):
+    for f in range(count):
+        spawn_food()
+
+def update_foods():
+    pass
 
 def draw_food():
-    for center in food:
-        pygame.draw.circle(screen, "mistyrose2", center, food_size)
+    for i in range(len(food_colors)):
+        food_pos = food[i]
+        food_color = food_colors[i]
+        pygame.draw.circle(screen, food_color ,food_pos, food_size)
 
 
-spawn_food()
+spawn_foods(500)
 
 while running:
     # poll for events
@@ -103,7 +121,7 @@ while running:
     # DRAW THE GAME
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("skyblue")
+    screen.fill(BACKGROUND_COLOR)
 
     draw_snake()
     draw_food()
